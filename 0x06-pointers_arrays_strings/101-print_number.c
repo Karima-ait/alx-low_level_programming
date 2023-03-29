2284b366f7ef9ea62d50aba456f4c4fc44aa2358
Karima-ait <karimaaitsidiali06@gmail.com>
2023-03-29 16:21:55 -0700

    Numbers have life

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,34 +1,26 @@
 #include "main.h"
 /**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
+ *print_number - prints an integer.
+ *only using the putchar function.
+ *noarrays and pointers.
+ *@n: integer to be printed.
+ *
+ *Return: void.
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
-
+	unsigned int num;
+/*check if number is negative*/
+	num = n;
 	if (n < 0)
 	{
 		_putchar(45);
-		m = n * -1;
+		num = -n;
 	}
-	else
+/* print number by recursion*/
+	if (num / 10)
 	{
-		m = n;
+		print_number(num / 10);
 	}
-
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	_putchar((num % 10) + '0');
 }
